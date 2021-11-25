793468851b7b0ba436551f571b6ed8c60fa8ad4c
kalsang <kaltse000@gmail.com>
2021-11-25 20:24:13 +0100

    Emotion ts-config until TypeScript 4.1

--- Component/Footer.tsx
+++ Component/Footer.tsx
@@ -1,57 +1,48 @@
+import { css } from '@emotion/react';
 import Link from 'next/link';
+
+const allButFooter = css`
+  display: flex;
+  bottom: 0;
+  justify-content: center;
+  width: 100%;
+  margin: 100px 0 0;
+`;
+const footerStyle = css`
+  margin-top: 20px;
+  padding: 20px;
+  a {
+    text-decoration: none;
+    color: white;
+    margin: 0 36px;
+    padding: 20px;
+    text-align: center;
+    justify-content: space-between;
+    width: 100%;
+    flex-direction: column;
+  }
+  a:hover {
+    color: #ff1966;
+  }
+`;
 
 export default function Footer() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        bottom: '0',
-        justifyContent: 'center',
-        width: '100%',
-        margin: '100px 0 0',
-        color: 'white',
-      }}
-    >
-      <Link href="/">
-        <a
-          style={{
-            margin: ' 0 36px',
-            padding: '20px',
-          }}
-        >
-          Data Protection
-        </a>
-      </Link>
-      <Link href="/">
-        <a
-          style={{
-            margin: ' 0 36px',
-            padding: '20px',
-          }}
-        >
-          Privacy Policy
-        </a>
-      </Link>
-      <Link href="/contact">
-        <a
-          style={{
-            margin: ' 0 36px',
-            padding: '20px',
-          }}
-        >
-          Contact
-        </a>
-      </Link>
-      <Link href="/contact">
-        <a
-          style={{
-            margin: ' 0 36px',
-            padding: '20px',
-          }}
-        >
-          Media Structure
-        </a>
-      </Link>
-    </div>
+    <footer css={allButFooter}>
+      <nav css={footerStyle}>
+        <Link href="/">
+          <a>Data Protection</a>
+        </Link>
+        <Link href="/">
+          <a>Privacy Policy</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+        <Link href="/contact">
+          <a style={{ textAlign: 'right' }}>Media Structure</a>
+        </Link>
+      </nav>
+    </footer>
   );
 }
